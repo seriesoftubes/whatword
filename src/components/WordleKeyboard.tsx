4f953aa1fd0997fe2c7bc4d9975eb3397255c59e
seriesoftubes <brianweiden@gmail.com>
2025-04-25 22:52:19 -0400

    Improve appearance of delete key. Refactor

--- src/components/WordleKeyboard.tsx
+++ src/components/WordleKeyboard.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { cn } from "@/lib/utils";
 import { LetterPresence } from '@/lib/types'
@@ -10,6 +9,17 @@ const KEY_ROWS = [
   ["BACK","Z","X","C","V","B","N","M","ENTER"]
 ];
 
+const KEY_WIDTHS = {
+  'ENTER': 75,
+  'BACK': 33,
+};
+
+const BACKGROUND_BY_PRESENCE: { [key: LetterPresence]: string } = {
+  "correct": "bg-wordle-correct text-white",
+  "present": "bg-wordle-present",
+  "absent": "bg-wordle-absent text-gray-400"
+};
+
 type KeyStatus = Record<string, LetterPresence | undefined>;
 
 interface WordleKeyboardProps {
@@ -17,31 +27,33 @@ interface WordleKeyboardProps {
   keyStatus: KeyStatus;
 }
 
+
 export const WordleKeyboard: React.FC<WordleKeyboardProps> = ({ onKey, keyStatus }) => (
   <div className="flex flex-col gap-2 px-12 w-full max-w-md mx-auto select-none">
     {KEY_ROWS.map((row, ri) => (
       <div key={ri} className="flex justify-center gap-1">
         {row.map((key) => {
-          let bg = "bg-wordle-key";
-          if (keyStatus[key] === "correct") bg = "bg-wordle-correct text-white";
-          else if (keyStatus[key] === "present") bg = "bg-wordle-present";
-          else if (keyStatus[key] === "absent") bg = "bg-wordle-absent text-gray-400";
-          if (key === "ENTER" || key === "BACK") bg = "bg-wordle-accent text-white";
-          const width = key == 'ENTER' ? 75 : 30;
+          const status = keyStatus[key];
+          let backgroundClass = "bg-wordle-key";
+          if (status in BACKGROUND_BY_PRESENCE) {
+            backgroundClass = BACKGROUND_BY_PRESENCE[status];
+          } else if (key === "ENTER" || key === "BACK") {
+            backgroundClass = "bg-wordle-accent text-white";
+          }
+          const width = KEY_WIDTHS[key] || 30;
 
           return (
             <button
               key={key}
               className={cn(
                 "flex-1 px-1 py-2 md:py-3 md:px-2 rounded-md md:rounded-lg font-semibold text-base md:text-lg transition-all duration-200",
-                bg,
+                backgroundClass,
                 "active:scale-50"
               )}
               style={{ minWidth: width }}
               onClick={() => onKey(key)}
               aria-label={key}
-              tabIndex={-1}
-            >
+              tabIndex={-1}>
               {key === "BACK" ? "⌫" : key}
             </button>
           );
