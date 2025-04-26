0b57513dc399b112d48f1bba4a65454a4db1af6d
seriesoftubes <brianweiden@gmail.com>
2025-04-26 17:27:24 -0400

    generate PWA manifest + SW

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { checker } from "vite-plugin-checker";
+import { VitePWA } from 'vite-plugin-pwa';
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
@@ -11,11 +12,57 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    checker({ typescript: true })
+    checker({ typescript: true }),
+    VitePWA({
+      workbox: {
+        globDirectory: 'dist/',
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg}',
+        ],
+        swDest: 'dist/sw.js',
+      },
+      // Updates the SW whenever a new one is detected
+      registerType: 'autoUpdate',
+      manifest: {
+        "id": "https://seriesoftubes.github.io/wordle",
+        "name": "Wordle",
+        "short_name": "Wordle",
+        "start_url": "/",
+        "scope": "/",
+        "lang": "en-US",
+        "display": "standalone",
+        "background_color": "#f0f0fb",
+        "theme_color": "#beadff",
+        "dir": "ltr",
+        "orientation": "portrait",
+        "categories": [
+          "games"
+        ],
+        "screenshots": [
+          {
+            "src": "screenshot-1.png",
+            "sizes": "1280x720",
+            "type": "image/png"
+          }
+        ],
+        "icons": [
+          {
+            "purpose": "any",
+            "sizes": "512x512",
+            "src": "favicon-512x512.png",
+            "type": "image/png"
+          }
+        ]
+      }
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    // Ensure sourcemaps are off for the generated SW in production
+    sourcemap: false,
   },
 }));
