c81a8565a9a05ca785dd8f32ad09092d1afcb9f9
seriesoftubes <brianweiden@gmail.com>
2025-05-25 10:12:44 -0400

    add missing semicolons

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 import { registerSW } from "virtual:pwa-register";
 
 
